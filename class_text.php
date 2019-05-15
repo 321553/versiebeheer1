e2f33a2777a29522b748b4303c391a5cb6436581
Leon Zethof <leonz.ionic@gmail.com>
2019-05-15 15:31:58 +0200

    wollahte

--- class_text.php
+++ class_text.php
@@ -1,11 +1,12 @@
 <?php
     class tekst {
         //fields
+        var $tekst = " lasagne ";
         var $tekst1 = " dit is een tekst met een alternatieve fact erin  ";
         var $tekst2 = " ik eeet boter. ";
         var $tekst3 = " ik eet water ";
         var $tekst4 = " en drink brood ";
-     
+        
     }
 
 
--- index.php
+++ index.php
@@ -4,10 +4,12 @@
 
     $tekst = new tekst();
 
+    echo $tekst->tekst;
     echo $tekst1->tekst1;
     echo $tekst->tekst2;
     echo $tekst->tekst3;
     echo $tekst->tekst4;
+
 
 ?>
 
