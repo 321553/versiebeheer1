14c501af0f761ee800369ed0ded9ae37088d711f
Leon Zethof <leonz.ionic@gmail.com>
2019-05-15 15:29:45 +0200

    dit is een niewue commit

--- class_text.php
+++ class_text.php
@@ -1,10 +1,11 @@
 <?php
     class tekst {
         //fields
-        var $tekst = " dit is een tekst met een alternatieve fact erin  ";
+        var $tekst1 = " dit is een tekst met een alternatieve fact erin  ";
         var $tekst2 = " ik eeet boter. ";
         var $tekst3 = " ik eet water ";
         var $tekst4 = " en drink brood ";
+     
     }
 
 
--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 
     $tekst = new tekst();
 
-    echo $tekst->tekst;
+    echo $tekst1->tekst1;
     echo $tekst->tekst2;
     echo $tekst->tekst3;
     echo $tekst->tekst4;
