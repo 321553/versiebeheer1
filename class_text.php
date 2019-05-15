0364124e5be235d7777a2b41b743f6fac3d38f23
Leon Zethof <leonz.ionic@gmail.com>
2019-05-15 16:06:53 +0200

    tekst 3 is weggehaald

--- class_text.php
+++ class_text.php
@@ -4,7 +4,7 @@
         var $tekst = " lasagne ";
         var $tekst1 = " dit is een tekst met een alternatieve fact erin  ";
         var $tekst2 = " ik eeet boter. ";
-        var $tekst3 = " ik eet water ";
+
         var $tekst4 = " en drink brood ";
         
     }
--- index.php
+++ index.php
@@ -5,9 +5,9 @@
     $tekst = new tekst();
 
     echo $tekst->tekst;
-    echo $tekst1->tekst1;
+    echo $tekst->tekst1;
     echo $tekst->tekst2;
-    echo $tekst->tekst3;
+
     echo $tekst->tekst4;
 
 
